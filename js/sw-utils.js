5bd8090225df2dd7ca91e477b710118f9910d446
Cristian Flores <cristianflores597@gmail.com>
2019-01-17 03:43:20 -0600

    Primer comit

---
+++ js/sw-utils.js
@@ -0,0 +1,16 @@
+
+// Guardar en cache dinamico
+function actualizaCacheDinamico(dynamicCache, req, resp) {
+    if( resp.ok ){
+        
+       return caches.open( dynamicCache ).then( cache => {
+
+            cache.put(req, resp.clone());
+            return resp.clone();
+
+        });
+    }
+    else {
+        return resp;
+    }
+}
